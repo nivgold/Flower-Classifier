86614fe66d9eadac02285967bd44e68cb1531a51
nivgold <nivgold@post.bgu.ac.il>
2020-06-08 14:19:17 +0300

    flowers labels

--- flowers.py
+++ flowers.py
@@ -2,11 +2,14 @@ import os
 from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
+
+from tensorflow.keras import layers, models
+from tensorflow.keras.utils import to_categorical
 
 flowers_path = "./flowers"
 
-#print(os.listdir(flowers_path))
 
 X = []
 labels = []
@@ -22,7 +25,7 @@ for flower_dir in os.listdir(flowers_path):
 			rgb_img = np.array(img.resize((250, 250)))
 
 			X.append(rgb_img)
-			labels.append(flower_name)
+			labels.append(str(flower_name))
 		except Exception as e:
 			print(e)
 			continue
@@ -31,13 +34,39 @@ for flower_dir in os.listdir(flowers_path):
 
 
 X = np.array(X)
-labels = np.array(labels)
-print(f'labels shape: {labels.shape}')
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(labels)
+y = to_categorical(y, 5)
 
 # preproccesing
 X = X/255
 
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.3, shuffle=True)
+# spliting to train test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True)
 
-print(X_train.shape)
-print(y_train.shape)
+# building the model
+model = models.Sequential()
+
+# Conv+Pooling 1
+model.add(layers.Conv2D(filters=64, kernel_size=(3,3) ,input_shape=(250,250,3), activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+
+# Conv+Pooling 2
+model.add(layers.Conv2D(filters=32, kernel_size=(3,3) ,activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2,2)))
+
+# Flatten
+model.add(layers.Flatten())
+
+# Fully connected layers
+model.add(layers.Dense(units=16, activation='relu'))
+model.add(layers.Dense(units=5, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print("learning...")
+
+model.fit(X_train, y_train, batch_size=2, epochs=10, validation_data=(X_test, y_test))
+
+print(model)
